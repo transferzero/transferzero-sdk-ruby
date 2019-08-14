cc72d1e22059da946918f71f29090dd7d49137e8
Travis CI <>
2019-08-14 11:56:14 +0000

    BTA-2070-update-ruby-sdk-versions-to-1.3.0) (#129)
    Update ruby sdks from 1.2.0 to 1.3.0

--- lib/transferzero-sdk/api_client.rb
+++ lib/transferzero-sdk/api_client.rb
@@ -35,7 +35,7 @@ module TransferZero
     # @option config [Configuration] Configuration for initializing the object, default to Configuration.default
     def initialize(config = Configuration.default)
       @config = config
-      @user_agent = "TransferZero-SDK/Ruby/1.2.0"
+      @user_agent = "TransferZero-SDK/Ruby/1.3.0"
       @default_headers = {
         'Content-Type' => 'application/json',
         'User-Agent' => @user_agent
